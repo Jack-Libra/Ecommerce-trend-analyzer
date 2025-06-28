3a5b29cbcbb38af23d04b9cbb5e9724dc0f39849
CHEN, CHUN-YEH <dafsf60804@gmail.com>
2025-06-28 19:20:50 +0800

    modified:   backend/middleware/cors.py

--- backend/middleware/cors.py
+++ backend/middleware/cors.py
@@ -4,7 +4,12 @@ from fastapi.middleware.cors import CORSMiddleware
 def get_cors_middleware(app):
     return CORSMiddleware(
         app,
-        allow_origins=["https://ecommerce-trend-analyzer-ib1e.vercel.app"],  # 部署時請改為你的前端網址
+        allow_origins=[
+            "https://ecommerce-trend-analyzer.vercel.app",
+            "https://ecommerce-trend-analyzer-ib1e.vercel.app",
+            "https://ecommerce-trend-analyzer-ib1e-git-main-jack-libras-projects.vercel.app",
+            "https://ecommerce-trend-analyzer-ib1e-pm9i9jzy1-jack-libras-projects.vercel.app"
+        ],  # 部署時請改為你的前端網址
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
