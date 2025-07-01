c927770a27f7b506adbfbe41cdfd10017a301105
CHEN, CHUN-YEH <dafsf60804@gmail.com>
2025-07-01 14:06:29 +0800

    modified:   frontend/src/app/api/admin/route.ts 	modified:   frontend/src/app/api/categories/route.ts 	modified:   frontend/src/app/api/keywords/route.ts 	modified:   frontend/src/app/api/products/route.ts

--- frontend/src/app/api/categories/route.ts
+++ frontend/src/app/api/categories/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // TODO: 改為 fetch('https://your-fastapi-url/categories')
-  const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/categories');
+  const res = await fetch('https://ecommerce-trend-analyzer.vercel.app/'+ '/categories');
   const data = await res.json();
   return NextResponse.json(data);
 }
--- frontend/src/app/api/products/route.ts
+++ frontend/src/app/api/products/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // fetch FastAPI /products/top
-  const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/products/top');
+  const res = await fetch('https://ecommerce-trend-analyzer.vercel.app/' + '/products/top');
   const data = await res.json();
   return NextResponse.json(data);
 }
